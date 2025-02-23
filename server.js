ace47c572a5171a968cbd16e7ff6b1e5f36b2a62
Harshach077 <harshach7523@gmail.com>
2025-02-24 00:02:12 +0530

    monogodb bug

--- server.js
+++ server.js
@@ -20,10 +20,9 @@ app.use(express.json());
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
+
+
 .then(() => console.log("✅ Connected to MongoDB Atlas"))
 .catch(err => {
     console.error("❌ MongoDB Connection Error:", err.message);
