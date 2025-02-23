04a195f857e82d163a179de5e4689efd0053f642
Harshach077 <harshach7523@gmail.com>
2025-02-23 11:30:45 +0530

    bug fix

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: "https://multispeciality-hospital-eight.vercel.app/",
+  origin: "https://multispeciality-hospital-eight.vercel.app",
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type"
 }));
